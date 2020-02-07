542ce8d41119b517f900e3f8cfb2c18bfb011658
Luthfi Goldiansyah Kusumajadi <Goldiansyah@gmail.com>
2020-02-07 09:50:54 +0700

    Migration & Image Upload

--- app/Http/Controllers/CuciMobilController.php
+++ app/Http/Controllers/CuciMobilController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class CuciController extends Controller
+class CuciMobilController extends Controller
 {
     public function index()
     {
@@ -15,58 +15,70 @@ class CuciController extends Controller
         // mengirim data pegawai ke view index
         return view('index', ['cucimobil' => $cucimobil]);
     }
-}
 
-public function tambah()
-{
- 
-	// memanggil view tambah
-	return view('tambah');
- 
-}
+	public function tambah()
+	{
+	
+		// memanggil view tambah
+		return view('tambah');
+	
+	}
 
-// method untuk insert data ke table pegawai
-public function store(Request $request)
-{
-	// insert data ke table pegawai
-	DB::table('cucimobil')->insert([
-        'cucimobil_plat_mobil' => $request->plat_mobil,
-        'cucimobil_nama_mobil' => $request->nama_mobil,
-        'cucimobil_jenis_cuci' => $request->jenis_cuci
-	]);
-	// alihkan halaman ke halaman pegawai
-	return redirect('/cucimobil');
- 
-}
+	// method untuk insert data ke table pegawai
+	public function store(Request $request)
+	{
+		// insert data ke table pegawai
+		DB::table('cucimobil')->insert([
+			'cucimobil_plat_mobil' => $request->plat_mobil,
+			'cucimobil_nama_mobil' => $request->nama_mobil,
+			'cucimobil_jenis_cuci' => $request->jenis_cuci,
+			'cucimobil_foto' => $request->foto
+		]);
 
-public function edit($id)
-{
-	// mengambil data pegawai berdasarkan id yang dipilih
-	$cucimobil = DB::table('cucimobil')->where('cucimobil_id',$id)->get();
-	// passing data pegawai yang didapat ke view edit.blade.php
-	return view('edit',['cucimobil' => $cucimobil]);
- 
-}
+		$this->validate($request, [
+			'featured_image' => 'required|file|max:7000', // max 7MB
+		]);
 
-// update data pegawai
-public function update(Request $request)
-{
+		$path = Storage::putFile(
+			'storage/Images',
+			$request->file('featured_image'),
+		);
+
+		// alihkan halaman ke halaman pegawai
+		return redirect('/cucimobil');
+	
+	}
+
+	public function edit($id)
+	{
+		// mengambil data pegawai berdasarkan id yang dipilih
+		$cucimobil = DB::table('cucimobil')->where('cucimobil_id',$id)->get();
+		// passing data pegawai yang didapat ke view edit.blade.php
+		return view('edit',['cucimobil' => $cucimobil]);
+	
+	}
+
 	// update data pegawai
-	DB::table('cucimobil')->where('cucimobil_id',$request->id)->update([
-		'cucimobil_plat_mobil' => $request->plat_mobil,
-		'cucimobil_nama_mobil' => $request->nama_mobil,
-		'cucimobil_jenis_cuci' => $request->jenis_cuci
-	]);
-	// alihkan halaman ke halaman pegawai
-	return redirect('/cucimobil');
-}
+	public function update(Request $request)
+	{
+		// update data pegawai
+		DB::table('cucimobil')->where('cucimobil_id',$request->id)->update([
+			'cucimobil_plat_mobil' => $request->plat_mobil,
+			'cucimobil_nama_mobil' => $request->nama_mobil,
+			'cucimobil_jenis_cuci' => $request->jenis_cuci
+		]);
+		// alihkan halaman ke halaman pegawai
+		return redirect('/cucimobil');
+	}
 
-// method untuk hapus data pegawai
-public function hapus($id)
-{
-	// menghapus data pegawai berdasarkan id yang dipilih
-	DB::table('cucimobil')->where('cucimobil_id',$id)->delete();
-		
-	// alihkan halaman ke halaman pegawai
-	return redirect('/cucimobil');
+	// method untuk hapus data pegawai
+	public function hapus($id)
+	{
+		// menghapus data pegawai berdasarkan id yang dipilih
+		DB::table('cucimobil')->where('cucimobil_id',$id)->delete();
+			
+		// alihkan halaman ke halaman pegawai
+		return redirect('/cucimobil');
+	}
+
 }
